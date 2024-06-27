4d52598dda39342f90e7b32d49b8a2951e8c76ca
raefknani <raef.knani@gmail.com>
2024-06-27 13:03:51 +0100

    safe save 2

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,7 +6,75 @@ import profile from "../assets/profile.png";
 function DefaultHome() {
   return (
     <div className="defaultHome">
-      <div className="AccountDashboard"></div>
+      <div className="AccountDashboard">
+        <div className="Account">
+          <div className="profileImage">
+            <img src={profile} alt="none" />
+          </div>
+          <div className="AccountData">
+            <div className="profileName">
+              <h1>Oluwatobi Olowu</h1>
+              <p>Bonus booster 24lv</p>
+              <div className="progress_barr">
+                <div className="progress"></div>
+              </div>
+              <div className="data_boxes">
+                <ul>
+                  <li>
+                    <div className="quizz_passed">
+                      <ul>
+                        <li>
+                          <i class="bi bi-flag-fill"></i>
+                        </li>
+                        <li className="iconContent">
+                          <p>27</p>
+                          <p>Quizz Passed</p>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="fastest_time">
+                      <ul>
+                        <li>
+                          <i class="bi bi-clock-fill"></i>
+                        </li>
+                        <li className="iconContent">
+                          <p>27min</p>
+                          <p>Fastest Time</p>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="correct_answers">
+                      <ul>
+                        <li>
+                          <i class="bi bi-check-circle-fill"></i>
+                        </li>
+                        <li className="iconContent">
+                          <p>200</p>
+                          <p>Correct Answers</p>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="second_part_category">
+          <ul>
+            <li>
+              <div className="achievements"></div>
+            </li>
+            <li>
+              <div className="feachered_category"></div>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div>
         <h1 className="QuizzTime greyy">Quiz Time</h1>
         <div className="Navbarr">
